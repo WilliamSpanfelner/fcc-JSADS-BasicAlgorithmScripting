e9fc5a68211b7f104fb10f993bbbade0065272d6
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-30 15:55:20 +0300

    Create vars for longestWordIndex & Length

--- 04_findString'sLongestWord.js
+++ 04_findString'sLongestWord.js
@@ -9,6 +9,10 @@ Your response should be a number.
 */
 function findLongestWordLength(str) {
     let words = str.split(' ');  // create array of words in string
+    let longestWordIndex = -1;  // the index of longest word
+    let longestWordLength = 0;  // the length of longest word
+
+
     console.log(words);
     return str.length;
 }
