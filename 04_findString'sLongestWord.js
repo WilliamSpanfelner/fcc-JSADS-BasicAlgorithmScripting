9c3da789d8e95f4f8fda197a4f2d8eea11364067
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-30 16:25:38 +0300

    Use map and max methods to return largest value

--- 04_findString'sLongestWord.js
+++ 04_findString'sLongestWord.js
@@ -8,19 +8,10 @@ Your response should be a number.
 
 */
 function findLongestWordLength(str) {
-    let words = str.split(' ');  // create array of words in string
-    let longestWordIndex = -1;  // the index of longest word
-    let longestWordLength = 0;  // the length of longest word
-
-    for (let i = 0; i < words.length; i++) {
-        const word = words[i];
-        if (word.length > longestWordLength) {
-            longestWordLength = word.length;
-            longestWordIndex = i;
-        }
-    }
-    console.log(longestWordIndex, longestWordLength);
-    return longestWordLength;
+    let words = str.split(' ').map((word) => word.length);  // map the word lengths directly
+    
+    return Math.max(...words);  // use the Math.max method to return largest value
 }
   
-findLongestWordLength("The quick brown fox jumped over the lazy dog");
+console.log(
+    findLongestWordLength("The quick brown fox jumped over the lazy dog"));
