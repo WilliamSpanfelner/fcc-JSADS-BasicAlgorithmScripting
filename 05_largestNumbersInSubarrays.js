0070114c4a2c75d4593e6e418760ccbe80188ba8
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-30 16:56:21 +0300

    Use map & max methods to create  max values array

--- 05_largestNumbersInSubarrays.js
+++ 05_largestNumbersInSubarrays.js
@@ -10,7 +10,8 @@ for loop, and access each member with array syntax arr[i].
 */
 
 function largestOfFour(arr) {
-    return arr;
+    const results = arr.map((element) => Math.max(...element));
+    return results;
 }
-  
+
 largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
