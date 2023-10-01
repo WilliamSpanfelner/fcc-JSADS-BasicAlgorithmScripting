58f2a8be99c0d902ae386da23a9dbb8e2adae831
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-01 17:53:58 +0300

    Use arrow func with map and indexOf

--- 09_findersKeepers.js
+++ 09_findersKeepers.js
@@ -7,8 +7,8 @@ means that given an element x, the 'truth test' is passed if
 func(x) is true. If no element passes the test, return undefined.
 */
 function findElement(arr, func) {
-    let num = 0;
+    let num = arr[arr.map(func).indexOf(true)];
     return num;
   }
   
-  findElement([1, 2, 3, 4], num => num % 2 === 0);
+  console.log(findElement([1, 2, 3, 4], num => num % 2 === 0));
