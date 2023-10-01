701b366938a01d354ad7a94d71680df2ef0afa41
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-01 17:32:07 +0300

    Check string length to max & truncate accordingly

--- 08_truncateAString.js
+++ 08_truncateAString.js
@@ -6,6 +6,14 @@ given maximum string length (second argument). Return the
 truncated string with a ... ending.
 */
 function truncateString(str, num) {
+    //1. determine str length and compare to num
+    if (str.length > num) {
+        //2. Drop characters after num characters & add elipsis;
+        const stringToKeep = str.slice(0, num) + '...';
+        // console.log(stringToKeep);
+        return stringToKeep
+    }
+    
     return str;
 }
 
