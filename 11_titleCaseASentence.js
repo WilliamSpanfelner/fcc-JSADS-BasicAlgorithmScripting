cc9d986309ffaaa5b3382035e3e4ee2cf6ba33c1
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-01 19:08:40 +0300

    Create array using split lowercase substrings
    then uppercase the first letter of each substring;
    finally join the elements to a single string.

--- 11_titleCaseASentence.js
+++ 11_titleCaseASentence.js
@@ -9,7 +9,25 @@ connecting words like the and of.
 */
 
 function titleCase(str) {
-    return str;
+    // Create an array from str using split
+    let strArray = str.split(' ');
+
+    // Ensure each element of the strArray is in lowercase
+    strArray = strArray.map(word => word.toLowerCase());
+
+    // Iterate over array to capitalise 1st letter in words
+    for (let i = 0; i < strArray.length; i++) {
+        const word = strArray[i];
+        strArray[i] = strArray[i].replace(word[0], word[0].toUpperCase());
+    }
+
+    // , strArray.join(' '));
+    return strArray.join(' ');
 }
 
-titleCase("I'm a little tea pot");
+const testData = [
+    "sHoRt AnD sToUt",
+    "HERE IS MY HANDLE HERE IS MY SPOUT",
+    "I'm a little tea pot",
+]
+console.log(titleCase(testData[2]));
