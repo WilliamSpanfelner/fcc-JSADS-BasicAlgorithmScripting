b17bb949059dbfd454744bc0f1efa1dcd1d32ce1
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-02 11:04:50 +0300

    Slice arr2 into new arr3 first to preserve arr2;
    then splice arr1 into arr3 as before

--- 12_sliceAndSplice.js
+++ 12_sliceAndSplice.js
@@ -13,10 +13,14 @@ the same after the function runs.
 */
 function frankenSplice(arr1, arr2, n) {
     /* Splice takes a starting index, number of items to delete,
-    and items to insert */
-    arr2.splice(n, 0, ...arr1);
-    console.log(arr1, arr2);
-    return arr2;
+    and items to insert 
+    Slice always returns a new array - so, to leave arr2 as new it
+    can be sliced initially.
+    */
+    let arr3 = arr2.slice(); 
+    arr3.splice(n, 0, ...arr1);
+    // console.log(arr1, arr2, arr3);
+    return arr3;
 }
 
 console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
