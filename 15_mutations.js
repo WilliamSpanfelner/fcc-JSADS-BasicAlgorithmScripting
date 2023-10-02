46fb6da6fd7f41b2a208d4ab27e6d110be3c162d
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-02 20:47:07 +0300

    Check if arr element 1 contains each
    letter of element 0.  If not return false.

--- 15_mutations.js
+++ 15_mutations.js
@@ -16,6 +16,10 @@ letters in line are present in Alien.
 function mutation(arr) {
     for (let index = 0; index < arr[0].length; index++) {
         const element = arr[0][index].toLowerCase();
+        if (!arr[1].toLowerCase().includes(element)) {
+            return false
+        }
+        console.log(element, arr[1].toLowerCase().includes(element));
     }
 }
 
