55f70916171ac773e206e1f2cabb593ed4d80034
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-03 10:12:43 +0300

    Initial commit

---
+++ 16_chunkyMonkey.js
@@ -0,0 +1,12 @@
+/*
+Chunky Monkey
+
+Write a function that splits an array (first argument) into groups the 
+length of size (second argument) and returns them as a two-
+dimensional array.
+*/
+function chunkArrayInGroups(arr, size) {
+    return arr;
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
